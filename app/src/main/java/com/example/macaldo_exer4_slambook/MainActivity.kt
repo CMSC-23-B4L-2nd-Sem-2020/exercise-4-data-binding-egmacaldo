a40b263b620e84694a4751ebf7b72ab476879462
egmacaldo <egmacaldo@up.edu.ph>
2020-03-02 18:46:11 +0800

    chore: replaced setContentView for binding

--- app/src/main/java/com/example/macaldo_exer4_slambook/MainActivity.kt
+++ app/src/main/java/com/example/macaldo_exer4_slambook/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.macaldo_exer4_slambook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.macaldo_exer4_slambook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
     }
 }
