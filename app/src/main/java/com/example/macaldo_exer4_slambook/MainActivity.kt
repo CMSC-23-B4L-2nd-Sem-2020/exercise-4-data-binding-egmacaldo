1aaf0b5544be7c095fa2b673f43a02fce95ea69f
egmacaldo <egmacaldo@up.edu.ph>
2020-03-02 19:22:06 +0800

    chore: added data creation

--- app/src/main/java/com/example/macaldo_exer4_slambook/MainActivity.kt
+++ app/src/main/java/com/example/macaldo_exer4_slambook/MainActivity.kt
@@ -8,10 +8,12 @@ import com.example.macaldo_exer4_slambook.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private val myValues: myValues = myValues()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-
+        binding.myValues = myValues
     }
 }
