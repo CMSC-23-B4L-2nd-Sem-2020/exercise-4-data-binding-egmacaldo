d665c820190454af2b686259c8d2fca29e466cc3
egmacaldo <egmacaldo@up.edu.ph>
2020-03-02 18:44:32 +0800

    feat: added binding object

--- app/src/main/java/com/example/macaldo_exer4_slambook/MainActivity.kt
+++ app/src/main/java/com/example/macaldo_exer4_slambook/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.macaldo_exer4_slambook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.macaldo_exer4_slambook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
