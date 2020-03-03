38ac08b9a1a07891256fd977eb30b7f6d7ba0dca
egmacaldo <egmacaldo@up.edu.ph>
2020-03-03 19:41:27 +0800

    feat: created function for done button

--- app/src/main/java/com/example/macaldo_exer4_slambook/MainActivity.kt
+++ app/src/main/java/com/example/macaldo_exer4_slambook/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.macaldo_exer4_slambook
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import com.example.macaldo_exer4_slambook.databinding.ActivityMainBinding
 
@@ -15,5 +18,51 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.myValues = myValues
+
+        binding.doneButton.setOnClickListener {
+            displayInput(it)
+        }
+    }
+
+    private fun displayInput(view: View) {
+        binding. apply {
+//            myName?.nickname = nicknameEdit.text.toString()
+//            invalidateAll()
+            nickname.visibility = View.GONE
+            name.visibility = View.GONE
+            age.visibility = View.GONE
+            birthday.visibility = View.GONE
+            phone.visibility = View.GONE
+            email.visibility = View.GONE
+            course.visibility = View.GONE
+            dream.visibility = View.GONE
+            crush.visibility = View.GONE
+            message.visibility = View.GONE
+            nicknameQ.visibility = View.GONE
+            nameQ.visibility = View.GONE
+            ageQ.visibility = View.GONE
+            birthdayQ.visibility = View.GONE
+            phoneQ.visibility = View.GONE
+            emailQ.visibility = View.GONE
+            courseQ.visibility = View.GONE
+            dreamQ.visibility = View.GONE
+            crushQ.visibility = View.GONE
+            messageQ.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameDisplay.visibility = View.VISIBLE
+            nameDisplay.visibility = View.VISIBLE
+            ageDisplay.visibility = View.VISIBLE
+            birthdayDisplay.visibility = View.VISIBLE
+            phoneDisplay.visibility = View.VISIBLE
+            emailDisplay.visibility = View.VISIBLE
+            courseDisplay.visibility = View.VISIBLE
+            dreamDisplay.visibility = View.VISIBLE
+            crushDisplay.visibility = View.VISIBLE
+            messageDisplay.visibility = View.VISIBLE
+        }
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
